cbce13f1542bdc0dfcb1d7735670eb0a1e8f9ec2
jsk41755 <jsk41755@naver.com>
2018-11-13 13:19:36 +0900

    trans.c write

--- trans.c
+++ trans.c
@@ -18,55 +18,55 @@ int instr_trans(char *op, char *args, char* mcode)
 	
 	if(src[0] == '%'){
 		if(dst[0] == '%')
-			printf("89\n");
+			strcpy(mcode,"89");
 	}//1.reg to reg
 	
-	else if(src[0] == '(' && src[ strlen(src)-1] ==')'){
+	else if(src[0] == '(' && src[strlen(src)-1] ==')'){
 		if(dst[0] == '%')
-			printf("8b\n");
+			strcpy(mcode,"8b");
 	}//2.mem to reg
-	
+
+	else if(src[0] == '-' && src[strchr(src) -1] == '(' && 
+	src[strlen(src)-1] == ')'){
+		if(dst[0] == '%')
+			strcpy(mcode,"8b")
+	}//3.mem to reg(addressing w/ disp.)
+
 	else if(src[0] == '0'){
 		if(dst[0] == '%')
-			printf("a1\n");
+			strcpy(mcode,"a1");
 	}//4.mem to reg(eax)
 	
 	else if(src[0] == '%'){
-		if(isxdigit(dst) == 1)
-			printf("a3\n");
+		if(dst[0] == '0')
+			strcpy(mcode,"a3");
 	}//5.reg(eax) to mem
 
 	else if(src[0] == '$'){
 		if(strcmp(dst == "%eax") == 0)
-			printf("B8\n");
+			strcpy(mcode,"b8");
 		
 		else if(strcmp(dst == "%ecx") == 0)
-			printf("B9\n");
+			strcpy(mcode,"b9")
 		
 		else if(strcmp(dst == "%edx") == 0)
-			printf("BA\n");
+			strcpy(mcode,"ba");
 
 		else if(strcmp(dst == "%ebx") == 0)
-			printf("BB\n");
+			strcpy(mcode,"bb");
 
 		else if(strcmp(dst == "%esp") == 0)
-			printf("BC\n");	
+			strcpy(mcode,"bc");
 		
 		else if(strcmp(dst == "%ebp") == 0)
-			printf("BD\n");
+			strcpy(mcode,"bd");
 	
 		else if(strcmp(dst == "%esi") == 0)
-			printf("BE\n");
+			strcpy(mcode,"be");
 
 		else if(strcmp(dst == "%edi") == 0)
-			printf("BF\n");
-		}	
-	
-	/********************************/
-    /*  							*/
-	/*    generate machine code     */
-    /*  							*/
-	/********************************/
+			strcpy(mcode,"bf");
+	}//6.immediate to reg	
 	
 	return 1;	
 }
