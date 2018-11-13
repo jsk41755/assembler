7bd7f34740fc64d02808218c2cc7d1a066394b41
jsk41755 <jsk41755@naver.com>
2018-11-13 16:56:30 +0900

    check.c mem to mem

--- check.c
+++ check.c
@@ -4,12 +4,61 @@ int is_valid(char *op, char *args)
 {
 	//printf("if valid, return 1\n");
 	//printf("otherwise, return 0\n");
+	char *src;
+        char *dst;
+        src = strtok(args, ",");
+        dst = strtok(NULL, ",");
 
-	/********************************/
-    /*  							*/
-	/*         syntax check         */
-    /*  							*/
-	/********************************/
+	if(src[0] == '(' && src[strlen(src)-1]	== ')'){
+		if(dst[0] == '(' && dst[strlen(dst)-1] == ')')
+			return 0;
+	}//case : 1 () -1 () 
+
+	if(src[0] == '(' && src[strlen(src)-1]  == ')'){
+		if(dst[0] == '-' && strchr(dst,'(') != NULL)
+			return 0;
+	}//case : 1() -2 -()
+
+	if(src[0] == '(' && src[strlen(src)-1]  == ')'){
+		if(dst[0] == '0')
+			return 0;
+	}//case : 1() -2 0
+
+	//==========================================================
+
+	if(src[0] == '-' && strchr(src,'(') != NULL){
+		if(dst[0] == '(' && dst[strlen(dst)-1] == ')')
+                        return 0;
+	}//case : 2 -() -1 ()
+	
+	 if(src[0] == '-' && strchr(src,'(') != NULL){
+		 if(dst[0] == '-' && strchr(dst,'(') != NULL)
+			return 0;
+	}//case : 2 -() -2 -()
+	
+	 if(src[0] == '-' && strchr(src,'(') != NULL){
+		if(dst[0] == '0')
+			return 0;
+	}//case : 2 -() -2 0
+	
+	//============================================================
+
+	if(src[0] == '0'){
+		if(dst[0] == '(' && dst[strlen(dst)-1] == ')')
+                        return 0;
+	}//case : 3 0 -1 ()
+	
+	if(src[0] == '0'){
+		if(dst[0] == '-' && strchr(dst,'(') != NULL)
+			return 0;
+	}//case : 3 0 -2 -()
+	
+	if(src[0] == '0'){
+		 if(dst[0] == '0')
+			return 0;
+	}//case : 3 0 -3 0
+		
+		
 	
 	return 1;
 }
