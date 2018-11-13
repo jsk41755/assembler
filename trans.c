5ae359f418f5224c8a6c5a8838f1b2329ef611dc
jsk41755 <jsk41755@naver.com>
2018-11-13 16:07:20 +0900

    trans.c completed

--- trans.c
+++ trans.c
@@ -26,10 +26,10 @@ int instr_trans(char *op, char *args, char* mcode)
 			strcpy(mcode,"8b");
 	}//2.mem to reg
 
-	else if(src[0] == '-' && src[strchr(src) -1] == '(' && 
-	src[strlen(src)-1] == ')'){
+	else if(src[0] == '-' && strchr(src,'(')!=NULL&& 
+		src[strlen(src)-1] == ')'){
 		if(dst[0] == '%')
-			strcpy(mcode,"8b")
+			strcpy(mcode,"8b");
 	}//3.mem to reg(addressing w/ disp.)
 
 	else if(src[0] == '0'){
@@ -43,28 +43,28 @@ int instr_trans(char *op, char *args, char* mcode)
 	}//5.reg(eax) to mem
 
 	else if(src[0] == '$'){
-		if(strcmp(dst == "%eax") == 0)
+		if(strcmp(dst,"%eax") == 0)
 			strcpy(mcode,"b8");
 		
-		else if(strcmp(dst == "%ecx") == 0)
-			strcpy(mcode,"b9")
+		else if(strcmp(dst,"%ecx") == 0)
+			strcpy(mcode,"b9");
 		
-		else if(strcmp(dst == "%edx") == 0)
+		else if(strcmp(dst, "%edx") == 0)
 			strcpy(mcode,"ba");
 
-		else if(strcmp(dst == "%ebx") == 0)
+		else if(strcmp(dst, "%ebx") == 0)
 			strcpy(mcode,"bb");
 
-		else if(strcmp(dst == "%esp") == 0)
+		else if(strcmp(dst , "%esp") == 0)
 			strcpy(mcode,"bc");
 		
-		else if(strcmp(dst == "%ebp") == 0)
+		else if(strcmp(dst ,"%ebp") == 0)
 			strcpy(mcode,"bd");
 	
-		else if(strcmp(dst == "%esi") == 0)
+		else if(strcmp(dst, "%esi") == 0)
 			strcpy(mcode,"be");
 
-		else if(strcmp(dst == "%edi") == 0)
+		else if(strcmp(dst, "%edi") == 0)
 			strcpy(mcode,"bf");
 	}//6.immediate to reg	
 	
